c40377c9519487e54f4b5f9719f68d48528d22cf
Souvik-Halder <souvik.halder2020@gift.edu.in>
2024-11-15 11:49:22 +0530

    Stack and heap example

---
+++ Oops_codes/StackHeapExample.java
@@ -0,0 +1,20 @@
+
+class Calculator{
+  int a=19;
+  public int add(int a,int b){
+    int c=a+b;
+    return c;
+
+  }
+}
+
+public class StackHeapExample {
+  public static void main(String[] args) {
+    int num=10;
+    Calculator cal=new Calculator();
+    int a=10;
+    int b=20;
+    int d=cal.add(a, b);
+    System.out.println(d);
+  }
+}
