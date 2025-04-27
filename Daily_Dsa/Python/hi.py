f3d756d718a5bd24342ec3674f139348718a39c6
Souvik-Halder <souvik.halder2020@gift.edu.in>
2025-04-27 23:58:22 +0530

    Basic Python programming

--- Daily_Dsa/Python/hi.py
+++ Daily_Dsa/Python/hi.py
@@ -1,4 +1,26 @@
-for i in range(6 - 1, -1, -1):
-    print(i)
+nums1 = [1, 2, 3, 4, 5]
+nums2 = [1, 2, 7]
+n1 = len(nums1)
+n2 = len(nums2)
+arr = []
+i = 0
+j = 0
 
-# 6 -> length Civick
+while i < n1 and j < n2:
+    if nums1[i] <= nums2[j]:
+        if len(arr) == 0 or arr[-1] != nums1[i]:
+            arr.append(nums1[i])
+        i += 1
+    else:
+        if len(arr) == 0 or arr[-1] != nums2[j]:
+            arr.append(nums2[j])
+        j += 1
+while i < n1:
+    if len(arr) == 0 or arr[-1] != nums1[i]:
+        arr.append(nums1[i])
+    i += 1
+while j < n2:
+    if len(arr) == 0 or arr[-1] != nums2[j]:
+        arr.append(nums2[j])
+    j += 1
+print(arr)
---
+++ New_Daily_Dsa_Track/Python/Number_System/Print_Every_digit_of_an_Number.py
@@ -0,0 +1,9 @@
+## Very Very Very Very important in number series
+
+n1 = int(input())
+
+temp = n1
+while temp > 0:
+    digit = temp % 10
+    print(digit)
+    temp = temp // 10
